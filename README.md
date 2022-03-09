b960f7061eafc31b52f1e77321f4895ff5424735
“shivapendem” <pendem.shiva89@gmail.com>
2022-03-09 16:36:22 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Description
+    npm package for private key to tron address.
+## Installation
+    `npm i tron-private-key-to-address --save`
+    ```
+    import {tron-private-key-to-address} from 'tron-private-key-to-address';
+    
+    ```
+
+## How to use
+```js
+
+    const tronPrivateKeyToAddress = require('tron-private-key-to-address')
+    console.log(tronPrivateKeyToAddress('4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'))
+     ```
+## Support
+    
+    Happy to add more, and need any updates, do get in touch on my telegram over [@chigovara](#https://t.me/chigovera)
---
+++ index.js
@@ -0,0 +1,18 @@
+const TronWeb = require('tronweb');
+const tronWeb = new TronWeb(
+  'http://127.0.0.1:9090',
+  'http://127.0.0.1:9090',
+  'http://127.0.0.1:9090',
+  'd6fbbf6eecffdb32172e391363a401f89617acb9dd01897b9fa180830a8a46b2',
+);
+
+    function privateKeyToTronAddress(inputString) {
+        var ethadddress=privateKeyToAddress(_users1[0].accounts_pvtkey);
+        const addressInHexFormat = ethadddress.replace('0x', '41');
+        const addressInBase58 = tronWeb.address.fromHex(addressInHexFormat);
+        //console.log(ethadddress , addressInBase58);
+        return addressInBase58;
+    };
+    
+
+module.exports = {privateKeyToTronAddress}
